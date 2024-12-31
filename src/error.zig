9732311b2c18c50812860402dcee7307cde89fb7
Johmaru <hatunehimeka@outlook.jp>
2025-01-01 01:02:12 +0900

    first commit

---
+++ src/error.zig
@@ -0,0 +1,31 @@
+const std = @import("std");
+
+pub const Error = error{
+    UnknownError,
+    UnknownCommand,
+    MissingArgument,
+};
+
+fn processCommand(args: [][:0]u8) Error!void {
+    if (args.len == 0) {
+        return Error.UnknownError;
+    } else if (args.len < 2) {
+        if (std.mem.eql(u8, args.ptr[1], "version")) {
+            return Error.UnknownError;
+        } else {
+            return Error.UnknownCommand;
+        }
+    } else {
+        return Error.UnknownCommand;
+    }
+}
+
+pub fn errorfn(args: [][:0]u8) void {
+    processCommand(args) catch |err| {
+        switch (err) {
+            Error.UnknownCommand => std.debug.print("Unknown command{s}\n", .{args.ptr[1]}),
+            Error.UnknownError => std.debug.print("Unknown error\n", .{}),
+            Error.MissingArgument => std.debug.print("Missing argument\n", .{}),
+        }
+    };
+}
